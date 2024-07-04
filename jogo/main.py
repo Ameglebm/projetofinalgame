5992cc05fc8bccfcae0f9c9b63a89ac1cdefd9c2
Ameglebm <amegleeu@gmail.com>
2024-07-04 03:00:41 -0300

    finalizado mais nao esta rodando em web

--- jogo/main.py
+++ jogo/main.py
@@ -6,94 +6,10 @@ pygame.font.init()
 
 lista = ["amar", "azul", "acordado", "amargura", "assistir", "apetecer", "ascender", "arrojado", 
     "aprender", "alcançar", "atribuir", "anarquia", "amistoso", "arretado", "ajudante", 
-    "atraente", "apoteose", "atrofiar", "adiantar", "arrancar", "afrontar", "abestado", 
-    "bizarro", "bem", "buscar", "basculho", "bastardo", "bivolt", "bom", "brado", "brasileiro", 
-    "burro", "bruma", "base", "brega", "bravo", "bonito", "bojo", "barganha", "beldade", 
-    "bondade", "bola", "bajulador", "belo", "brasil", "bolo", "burrice", "bobo", "bruços", 
-    "bônus", "biodiversidade", "cultura", "confiança", "como", "conserto", "circunstância", 
-    "caos", "consequência", "casual", "cinismo", "cético", "conveniência", "compaixão", 
-    "consonância", "cordial", "compartilhar", "complexo", "céu", "clichê", "critério", 
-    "coragem", "consoante", "carência", "canalha", "coerente", "custódia", "colapso", "casa", 
-    "caridade", "conhecer", "complexidade", "conforme", "descobrir", "defasagem", "desalento", 
-    "diplomata", "democrata", "divertido", "destituir", "dezesseis", "discordar", "distraído", 
-    "devolução", "disfarçar", "devastado", "desonesto", "dilapidar", "desabafar", "denunciar", 
-    "desbocado", "devaneios", "despedida", "desamparo", "descrença", "desgastar", "debandada", 
-    "desanimar", "deliciosa", "decrescer", "dessagrar", "desatacar", "elo", "entusiasta", 
-    "estima", "emissão", "extorquir", "esboço", "extinguir", "explicar", "errante", "exalar", 
-    "executar", "exclusão", "elegibilidade", "erudito", "eficaz", "estúpido", "expedido", 
-    "exótico", "estereótipo", "fragmentado", "fascinante", "flanco", "factual", "fragrância", 
-    "fito", "férias", "fiel", "funcionalidade", "feminino", "fechar", "fascínio", "fatores", 
-    "franqueza", "fofoca", "fugir", "fenda", "fustigado", "fascinar", "fraternal", "flor", 
-    "fundamentar", "ficha", "geocentrismo", "geringonça", "gesticular", "gene", "germânico", 
-    "gelo", "gerenciamento", "gelatina", "germinação", "geek", "geleira", "gerencial", "gel", 
-    "gelificante", "geba", "gesticulação", "gelar", "germinal", "geomorfológico", "geminação", 
-    "geotermômetro", "gertrudes", "geologicamente", "georreferenciar", "geniculado", "gerontológico", 
-    "gemar", "genialmente", "geminípara", "gengibirra", "honorário", "hidrópico", "herético", 
-    "hiato", "haja", "honorários", "honrar", "higiene", "heterodoxo", "hígido", "herança", 
-    "homeostase", "higienizar", "horrível", "heteronomia", "humanização", "habitat", "homeopático", 
-    "hospício", "hansa", "herdar", "hibridismo", "hotel", "honroso", "hereditariedade", "hilariante", 
-    "honorável", "hostilizado", "homizio", "hiância", "imanente", "insípido", "indiferença", 
-    "intrínseca", "interesse", "instância", "insano", "intersecção", "intempestivo", "instável", 
-    "idiossincrático", "intolerância", "inútil", "inevitável", "impor", "imiscuir", 
-    "imperativo", "intervir", "incrédulo", "indissociável", "imaginação", "imagem", "inescrupuloso", 
-    "ilusão", "injúria", "insolência", "ilustre", "jazigo", "jactar", "jogo", "jovialidade", 
-    "justa", "justificação", "justaposto", "jamanta", "judiação", "jesuíta", "jazida", "javé", 
-    "jogador", "jerivá", "jangada", "janeiro", "jarro", "joana", "jabiraca", "jandaia", 
-    "jogatina", "julho", "junípero", "jubilamento", "joule", "jurígeno", "jararaca", "jaca", 
-    "jarretar", "juncar", "judas", "jardar", "jejuar", "jogado", "judicativo", "jongo", 
-    "japão", "karaokê", "ketchup", "kit", "kitchenette", "kickboxing", "kung fu", "kart", 
-    "kartódromo", "kimono", "lua", "lembrança", "literalmente", "lucidez", "lastro", "lástima", 
-    "laico", "lânguido", "ladainha", "linguagem", "ludibriado", "lenda", "louvor", "litigar", 
-    "leite", "legítimo", "ladrão", "lassidão", "limitação", "liberal", "lema", "libido", 
-    "light", "loucura", "lindeiro", "latino", "lacustre", "lapela", "livre-arbítrio", "libertino", 
-    "légua", "levantamento", "lábia", "linhagem", "litigância", "logia", "limpo", "lutar", 
-    "lúbrico", "mesmo", "menção", "método", "martírio", "mostrar", "mercê", "melindre", 
-    "maroto", "mediador", "moderado", "monogâmico", "memória", "miliciano", "manutenção", 
-    "manter", "marrento", "moção", "misticismo", "morar", "metafísica", "mutável", "mameluco", 
-    "modesta", "modo", "mentor", "manifesto", "medida", "mancomunado", "moralista", "metonímia", 
-    "massivo", "murmúrio", "monastério",     "mau", "nascimento", "nativo", "natural", "navegante", "necessidade", "neto", "nível", "nobre", "nadadeira", "narguilé", "navalha", "nave", "navio", "nebulizador", "niple", "obcecado", "otimizar", "oneroso", "outro", "opressão", "obtuso", "orla", "oblação", "onerar", "obstruir", "obstrução", "opcional", "orvalho", "olá", "obra", "oásis", 
-    "ordenado", "oferta", "online", "ocasionalmente", "oficial", "ocupado", "ordeiro", 
-    "obrigada", "ourives", "osso", "oportunismo", "opróbio", "obediente", "off", "objetar", 
-    "outdoor", "originário", "organizado", "oscular", "olfato", "operacional", "paciência", 
-    "problema", "pejorativo", "preceito", "proposição", "paralelo", "primordial", "pragmatismo", 
-    "pesar", "perseverar", "providência", "pela", "prazer", "postergar", "pudor", "pitoresco", 
-    "patente", "prosélito", "parecer", "preponderante", "ponderado", "propiciar", "práxis", 
-    "proeza", "puder", "pleito", "passiva", "preterido", "preservar", "prosperidade", "pulha", 
-    "pátria", "proposta", "perfazer", "patológico", "quadro", "qualidade", "quase", "quatro", 
-    "quebrado", "queda", "queijo", "queimar", "queixa", "quente", "querer", "quero", "questão", 
-    "química", "quilate", "quilômetro", "quimera", "quindim", "quiosque", "quociente", "quota", 
-    "quintal", "quinhão", "quarto", "quieto", "quiver", "quorum", "quente", "quinteto", "quartzo", 
-    "quadrado", "quasar", "quebra", "quadro", "qualquer", "quartel", "queijo", "química", "química", 
-    "quimono", "quilombo", "quintal", "querido", "querido", "quilograma", "quarentena", "quinhão", 
-    "quadrúpede", "quarto", "rabisco", "rabo", "raça", "rachado", "radar", "rádio", "rádix", "raia", 
-    "raio", "raiva", "raiz", "rajada", "ralar", "ralo", "ramal", "rã", "ranger", "ranho", "ranzinza", 
-    "rapadura", "rapariga", "rapaz", "rapel", "rápido", "rapina", "raposa", "raquete", "rasante", 
-    "rasgar", "raso", "rastro", "ratificar", "rato", "razoável", "razão", "realeza", "realidade", 
-    "rebanho", "rebelde", "recado", "receber", "recessão", "recibo", "recipiente", "recitar", 
-    "recobrar", "recolher", "recordar", "recurso", "redemoinho", "sábio", "sabão", "sabedoria", 
-    "saber", "saborear", "sabor", "sacada", "saco", "sacrifício", "sádico", "sagacidade", "saga", 
-    "sagrado", "saída", "salada", "salário", "saldo", "salgado", "saliva", "salmão", "salmo", 
-    "salpicado", "salsa", "saltador", "saltar", "salto", "samba", "sâmbico", "sanatório", "sangue", 
-    "santa", "santo", "sapa", "sapo", "saque", "saracoteio", "sarjeta", "sarna", "sarna", "sátira", 
-    "satisfação", "sazonal", "saudável", "saudade", "saudar", "saúde", "saudoso", "seara", "sebo", 
-    "seção", "tabaco", "tábua", "taco", "tática", "taberneiro", "tabuleiro", "tábula", "tacão", 
-    "táctil", "tagarela", "tamanho", "tamanco", "tâmara", "tampo", "tampar", "tandem", "tanga", 
-    "tango", "tanque", "tanto", "tarde", "tarefa", "tártaro", "tarugo", "tascar", "tatu", "tatuagem", 
-    "tatuar", "teatro", "tecido", "tecla", "teclar", "tecnologia", "teimoso", "telhado", "telha", 
-    "telescópio", "telúrico", "tempo", "tenda", "tênis", "tenor", "tentar", "tentação", "tênue", 
-    "teor", "teórico", "ter", "terra", "terraço", "ubíquo", "uberaba", "ubá", "ubi", "ubíqua", "ubíquas", 
-    "ubre", "úlcera", "ulisse", "ultimato", "último", "umidade", "único", "unidade", "uniforme", 
-    "unificar", "unilateral", "unir", "universal", "universidade", "urbano", "urgente", "urna", 
-    "urubu", "urze", "usufruir", "usual", "usura", "usurpador", "utilidade", "utilitário", "utilizar", 
-    "utopia", "utrículo", "uva", "vadio", "validação", "vapor", "vasta", "veloz", "vênia", "veneno", 
-    "verídico", "vermelho", "verão", "vestido", "vexatório", "viabilidade", "xadrez", "xale", "xangô", 
-    "xará", "xarope", "xeque", "xícara", "xiita", "xilogravura", "xodó", "xoxo", "xote", "xuxa", 
-    "xícara", "xaxim", "xênon", "xerife", "xerox", "xexéu", "xará", "xadrezista", "xamã", "xamânico", 
-    "xanadu", "xará", "xará", "xávega", "xavante", "xeque-mate", "xenofobia", "xereca", "xerox", 
-    "xicara", "xilogravura", "xilofone", "xiririca", "xixi", "xodó", "xucro", "xuxu", "zoada", "zombar", 
-    "zoológico", "zoom", "zoonomia", "zoomorfismo", "zorra", "zulu", "zumbido", "zumbi", "zumbir", "zuílo", "zulu", "zumbi", "zunir", "zunzum", "zunzunzum", "zuízo", "zúrico", "zuzum", "zíper", "zózimo", "zuísmo", "zuíz", "zóxis", "zuízo", "zíper", "zoada", "zoa", "zoada", "zoada", "zoada", "zoada", "zoada", "zoada"]
+    "atraente"]
 
 contador = 0
+
 def sortear_nome():
     return random.choice(lista).upper()
 
@@ -145,6 +61,22 @@ def mostrar_letras_erradas(tela, letras_erradas):
     texto = fonte_pequena.render("Erros: " + ", ".join(letras_erradas), True, cor_texto)
     tela.blit(texto, (10, 10))
 
+def desenhar_botao(tela, texto, pos, tamanho):
+    botao = pygame.Rect(pos, tamanho)
+    pygame.draw.rect(tela, cor_texto, botao, border_radius=20)
+    texto_render = fonte_pequena.render(texto, True, cor_fundo)
+    tela.blit(texto_render, (pos[0] + (tamanho[0] - texto_render.get_width()) // 2, pos[1] + (tamanho[1] - texto_render.get_height()) // 2))
+    return botao
+
+estado_jogo = "menu"
+
+def resetar_jogo():
+    global sorte, letras_acertadas, letras_erradas, estado_jogo
+    sorte = sortear_nome()
+    letras_acertadas = []
+    letras_erradas = []
+    estado_jogo = "jogando"
+
 gameloop = True
 while gameloop:
     tela.fill(cor_fundo)
@@ -152,25 +84,65 @@ while gameloop:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             gameloop = False
-        elif evento.type == pygame.KEYDOWN:
-            letra = evento.unicode.upper()
-            if letra.isalpha() and len(letra) == 1:
-                if letra in sorte and letra not in letras_acertadas:
-                    letras_acertadas.append(letra)
-                elif letra not in sorte and letra not in letras_erradas:
-                    letras_erradas.append(letra)
-    
-    mostrar_palavra(tela, sorte, letras_acertadas)
-    mostrar_letras_erradas(tela, letras_erradas)
-    desenhar_forca(tela, len(letras_erradas))
 
-    if set(sorte) <= set(letras_acertadas):
+        elif evento.type == pygame.KEYDOWN:
+            if estado_jogo == "jogando":
+                letra = evento.unicode.upper()
+
+                if letra.isalpha() and len(letra) == 1:
+                    if letra in sorte and letra not in letras_acertadas:
+                        letras_acertadas.append(letra)
+                    elif letra not in sorte and letra not in letras_erradas:
+                        letras_erradas.append(letra)
+
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if estado_jogo == "menu":
+
+                if botao_play.collidepoint(evento.pos):
+                    resetar_jogo()
+
+            elif estado_jogo == "venceu":
+                if botao_continuar.collidepoint(evento.pos):
+                    contador += 1
+
+                    resetar_jogo()
+            elif estado_jogo == "perdeu":
+                if botao_tentar_novamente.collidepoint(evento.pos):
+                    contador = 0
+                    resetar_jogo()
+
+    if estado_jogo == "menu":
+        botao_play = desenhar_botao(tela, "Play", (350, 250), (100, 50))
+
+    elif estado_jogo == "jogando":
+        mostrar_palavra(tela, sorte, letras_acertadas)
+        mostrar_letras_erradas(tela, letras_erradas)
+        desenhar_forca(tela, len(letras_erradas))
+
+        if set(sorte) <= set(letras_acertadas):
+            estado_jogo = "venceu"
+
+        elif len(letras_erradas) >= 6:
+            estado_jogo = "perdeu"
+    elif estado_jogo == "venceu":
+        
         texto_vitoria = fonte_media.render("Você Venceu!", True, cor_texto)
-        tela.blit(texto_vitoria, (500 - texto_vitoria.get_width() // 2, 300))
-    elif len(letras_erradas) >= 6:
+        tela.blit(texto_vitoria, (400 - texto_vitoria.get_width() // 2, 250))
+
+        botao_continuar = desenhar_botao(tela, "Continuar", (350, 400), (100, 50))
+
+        texto_pontos = fonte_pequena.render(f"Pontos: {contador}", True, cor_texto)
+
+        tela.blit(texto_pontos, (350, 350))
+    elif estado_jogo == "perdeu":
         texto_derrota = fonte_pequena.render(f"Você Perdeu! Era: {sorte}", True, cor_texto)
-        tela.blit(texto_derrota, (500 - texto_derrota.get_width() // 2, 250))
-   
+        tela.blit(texto_derrota, (400 - texto_derrota.get_width() // 2, 250))
+
+        botao_tentar_novamente = desenhar_botao(tela, "Tentar Novamente", (300, 400), (200, 50))
+
+        texto_pontos = fonte_pequena.render(f"Pontos: {contador}", True, cor_texto)
+        tela.blit(texto_pontos, (350, 350))
+    
     pygame.display.flip()
 
 pygame.quit()
