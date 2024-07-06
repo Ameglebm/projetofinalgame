b2117c3732068b52c7805375ec183bb5828c8dfe
Ameglebm <amegleeu@gmail.com>
2024-07-06 15:33:55 -0300

    Paginas de cadastro e index com path pronto, terminar as outras depois, tambem passado para templates o html e o css com images para static

--- game_log/urls.py
+++ game_log/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from game_logs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
--- game_logs/views.py
+++ game_logs/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'paginas/index.html')
+
+
+def cadastro(request):
+    return render(request, 'paginas/cadastro.html')
 
 # Create your views here.
