d51267f2a8dbf4ed4920f50f2b6d560cd2e40b53
Ameglebm <amegleeu@gmail.com>
2024-07-08 06:03:52 -0300

    banco de dados se comunicando com a pagina de cadastro e tambem a pagina de login, ainda precisa de uns ajustes, tambem arrumar as divs pois quando coloquei o form actiom acabou mudando o formulario

--- game_log/urls.py
+++ game_log/urls.py
@@ -5,8 +5,9 @@ from game_logs import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('cadastro/', views.cadastro, name='cadastro'),
+    path('cadastre/', views.cadastre, name='cadastre'),
     path('contato/', views.contato, name='contato'),
     path('login/', views.login, name='login'),
+    path('plataforma/', views.plataforma, name='plataforma'),
 ]
 
